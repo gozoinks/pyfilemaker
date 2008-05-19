d68d459e37d812d9994cef285e4dc06a386c952b
klokan <klokan@0857e5ee-4a4d-0410-be31-bb5ddc5f01a4>
2008-05-19 13:33:43 +0000

    Initial import of PyFileMaker tree into the svn.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyFileMaker',
+      version='2.5',
+      description='Python Object Interface to FileMaker Server / FileMaker’s XML interface',
+      author='Pieter Claerhout, Klokan Petr Pridal',
+      author_email='pieter@yellowduck.be, klokan@klokan.cz',
+      url='http://code.google.com/p/pyfilemaker/',
+      packages=['PyFileMaker'],
+     )
