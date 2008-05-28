8d983c1afeacf90e7fb7f30826e95b6f71110342
klokan <klokan@0857e5ee-4a4d-0410-be31-bb5ddc5f01a4>
2008-05-28 14:00:57 +0000

    USAGE.txt is inserted into PyFileMaker DocString. For better functionality of help() and pydoc and PyPI.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from PyFileMaker import __version__
+from PyFileMaker import __version__, __doc__
 
 setup(name='PyFileMaker',
       version=__version__,
       description='Python Object Wrapper for FileMaker Server XML Interface',
+      long_description=__doc__,
       author='Pieter Claerhout, Klokan Petr Pridal',
       author_email='pieter@yellowduck.be, klokan@klokan.cz',
       license='http://www.opensource.org/licenses/bsd-license.php',
